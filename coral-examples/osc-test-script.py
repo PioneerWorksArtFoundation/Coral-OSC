7d23ac91c7576b6d780a6063955abc89151e4f1f
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-01-22 11:47:19 -0500

    Use Click to add arguments into the test client script

--- coral-examples/osc-test-script.py
+++ coral-examples/osc-test-script.py
@@ -1,9 +1,36 @@
-from oscpy.client import OSCClient
+import click
 from time import sleep
+from oscpy.client import OSCClient
 
-osc = OSCClient('192.168.100.89', 8080)
-while(True):
-    messageText = "hello".encode("utf8")
-    osc.send_message(b'/ping', messageText)
-    print("Sent message!\n\n")
-    sleep(1)
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    '-ip', '--ip-address', 'ip_address',
+    default=None,
+    required=True,
+    help="Used to specify the port that's listening for request on the OSC server handling these OSC messages."
+)
+@click.option(
+    '-p', '--port', 'port',
+    default=None,
+    required=True,
+    help="Used to specify the port that's listening for request on the OSC server handling these OSC messages."
+)
+@click.option(
+    '--path', 'path',
+    default='/',
+    required=True,
+    help="Path (endpoint) at which the OSC server is listening for requests."
+)
+def main(ip_address, port, path):
+    osc = OSCClient(ip_address, int(port), encoding="utf8")
+    while(True):
+        osc.send_message(path, "hello")
+        print("Sent message!\n\n")
+        sleep(1)
+
+
+if __name__ == '__main__':
+    main()
